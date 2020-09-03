3d208ffe6304381db2d722a1217f44f549d9cb22
Juan Pablo Correa Puerta <jp.correap@uniandes.edu.co>
2020-09-03 15:59:06 -0500

    Updates

---
+++ precache-manifest.6968c828beeeb3ad5c847b6a460749ba.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54e17a1dbca4acf387e7fc0c1dbf594b",
+    "url": "/jpcorreap.github.io/index.html"
+  },
+  {
+    "revision": "b353269c98357eb577fa",
+    "url": "/jpcorreap.github.io/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "f2284bfbda7198f2ef63",
+    "url": "/jpcorreap.github.io/static/css/main.e2bb088e.chunk.css"
+  },
+  {
+    "revision": "b353269c98357eb577fa",
+    "url": "/jpcorreap.github.io/static/js/2.096448a2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jpcorreap.github.io/static/js/2.096448a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2284bfbda7198f2ef63",
+    "url": "/jpcorreap.github.io/static/js/main.ab616c5b.chunk.js"
+  },
+  {
+    "revision": "d404d85fb63271b98e3e",
+    "url": "/jpcorreap.github.io/static/js/runtime-main.45e6d879.js"
+  },
+  {
+    "revision": "d938e7603d377de31ec53c74fc265c4d",
+    "url": "/jpcorreap.github.io/static/media/ECN.d938e760.png"
+  },
+  {
+    "revision": "20e82a64ee76b735387e2ce3076d6fe5",
+    "url": "/jpcorreap.github.io/static/media/EN_Juan Pablo Correa Puerta.20e82a64.pdf"
+  },
+  {
+    "revision": "c6a8785426e72bdcddf48a28fa0b73a4",
+    "url": "/jpcorreap.github.io/static/media/Github_logo.c6a87854.svg"
+  },
+  {
+    "revision": "5adebe89f0ee317f8541bd35fe35d807",
+    "url": "/jpcorreap.github.io/static/media/fondo.5adebe89.svg"
+  },
+  {
+    "revision": "ae74bc4758108fca160627f4b4959559",
+    "url": "/jpcorreap.github.io/static/media/fondo_sm.ae74bc47.svg"
+  },
+  {
+    "revision": "a144a1858f10fdf2ec42a9f378900087",
+    "url": "/jpcorreap.github.io/static/media/gmail.a144a185.png"
+  },
+  {
+    "revision": "30e06c16c19024757bf941f17facff2e",
+    "url": "/jpcorreap.github.io/static/media/itesm.30e06c16.png"
+  },
+  {
+    "revision": "8f1e0f0f53b3f3e7035872a5f62db465",
+    "url": "/jpcorreap.github.io/static/media/teotihuacan.8f1e0f0f.jpg"
+  },
+  {
+    "revision": "358903239db0b801f96ad67413265fad",
+    "url": "/jpcorreap.github.io/static/media/uniandes.35890323.png"
+  },
+  {
+    "revision": "afbb2bf9ebc5c75fd5d7e3c862605d47",
+    "url": "/jpcorreap.github.io/static/media/upb.afbb2bf9.png"
+  }
+]);
